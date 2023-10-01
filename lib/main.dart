1be774c23baae9ba4416a3125f47d9399d10c4ec
Joe X Zhu <zhuxun_cn@yahoo.com>
2023-10-01 11:27:31 +0800

    Revision that compile-able

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:location_permissions/location_permissions.dart';
 import 'dart:io' show Platform;
+import "package:hex/hex.dart";
 
 // This flutter app demonstrates an usage of the flutter_reactive_ble flutter plugin
 // This app works only with BLE devices which advertise with a Nordic UART Service (NUS) UUID
-Uuid _UART_UUID = Uuid.parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
-Uuid _UART_RX   = Uuid.parse("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
-Uuid _UART_TX   = Uuid.parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+// Uuid _UART_UUID = Uuid.parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+// Uuid _UART_RX = Uuid.parse("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+// Uuid _UART_TX = Uuid.parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+Uuid _UART_UUID = Uuid.parse("4157B00B-D129-AEB5-859B-D547E30FFCB1");
+Uuid _UART_RX = Uuid.parse("4157B611-D129-AEB5-859B-D547E30FFCB1");
+Uuid _UART_TX = Uuid.parse("4157B612-D129-AEB5-859B-D547E30FFCB1");
 
 void main() {
   runApp(MyApp());
@@ -29,7 +34,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, this.title = ''}) : super(key: key);
   final String title;
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -38,13 +43,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final flutterReactiveBle = FlutterReactiveBle();
   List<DiscoveredDevice> _foundBleUARTDevices = [];
-  StreamSubscription<DiscoveredDevice> _scanStream;
-  Stream<ConnectionStateUpdate> _currentConnectionStream;
-  StreamSubscription<ConnectionStateUpdate> _connection;
-  QualifiedCharacteristic _txCharacteristic;
-  QualifiedCharacteristic _rxCharacteristic;
-  Stream<List<int>> _receivedDataStream;
-  TextEditingController _dataToSendText;
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late Stream<ConnectionStateUpdate> _currentConnectionStream;
+  late StreamSubscription<ConnectionStateUpdate> _connection;
+  late QualifiedCharacteristic _txCharacteristic;
+  late QualifiedCharacteristic _rxCharacteristic;
+  late Stream<List<int>> _receivedDataStream;
+  late TextEditingController _dataToSendText;
   bool _scanning = false;
   bool _connected = false;
   String _logTexts = "";
@@ -61,12 +66,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _sendData() async {
-      await flutterReactiveBle.writeCharacteristicWithResponse(_rxCharacteristic, value: _dataToSendText.text.codeUnits);
+    var hex = 022;
+    await flutterReactiveBle.writeCharacteristicWithResponse(_rxCharacteristic,
+        value: hexToUint8List(_dataToSendText.text));
+  }
+
+  Uint8List hexToUint8List(String hex) {
+    if (hex.length % 2 != 0) {
+      throw 'Odd number of hex digits';
+    }
+    var l = hex.length ~/ 2;
+    var result = Uint8List(l);
+    for (var i = 0; i < l; ++i) {
+      var x = int.parse(hex.substring(2 * i, 2 * (i + 1)), radix: 16);
+      if (x.isNaN) {
+        throw 'Expected hex string';
+      }
+      result[i] = x;
+    }
+    return result;
   }
 
   void onNewReceivedData(List<int> data) {
     _numberOfMessagesReceived += 1;
-    _receivedData.add( "$_numberOfMessagesReceived: ${String.fromCharCodes(data)}");
+
+    HexEncoder enc = HexEncoder(upperCase: true);
+    _receivedData.add("$_numberOfMessagesReceived: ${enc.convert(data)}");
+    // .add("$_numberOfMessagesReceived: ${String.fromCharCodes(data)}");
     if (_receivedData.length > 5) {
       _receivedData.removeAt(0);
     }
@@ -86,15 +112,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> showNoPermissionDialog() async => showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) => AlertDialog(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) => AlertDialog(
           title: const Text('No location permission '),
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
                 const Text('No location permission granted.'),
-                const Text('Location permission is required for BLE to function.'),
+                const Text(
+                    'Location permission is required for BLE to function.'),
               ],
             ),
           ),
@@ -107,45 +134,40 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-    );
+      );
 
   void _startScan() async {
-    bool goForIt=false;
+    bool goForIt = false;
     PermissionStatus permission;
     if (Platform.isAndroid) {
       permission = await LocationPermissions().requestPermissions();
-      if (permission == PermissionStatus.granted)
-        goForIt=true;
+      if (permission == PermissionStatus.granted) goForIt = true;
     } else if (Platform.isIOS) {
-      goForIt=true;
+      goForIt = true;
     }
-    if (goForIt) { //TODO replace True with permission == PermissionStatus.granted is for IOS test
+    if (goForIt) {
+      //TODO replace True with permission == PermissionStatus.granted is for IOS test
       _foundBleUARTDevices = [];
       _scanning = true;
       refreshScreen();
-      _scanStream =
-          flutterReactiveBle.scanForDevices(withServices: [_UART_UUID]).listen((
-              device) {
-            if (_foundBleUARTDevices.every((element) =>
-            element.id != device.id)) {
-              _foundBleUARTDevices.add(device);
-              refreshScreen();
-            }
-          }, onError: (Object error) {
-            _logTexts =
-                "${_logTexts}ERROR while scanning:$error \n";
-            refreshScreen();
-          }
-          );
-    }
-    else {
+      _scanStream = flutterReactiveBle
+          .scanForDevices(withServices: [/* _UART_UUID */]).listen((device) {
+        if (_foundBleUARTDevices.every((element) => element.id != device.id)) {
+          _foundBleUARTDevices.add(device);
+          refreshScreen();
+        }
+      }, onError: (Object error) {
+        _logTexts = "${_logTexts}ERROR while scanning:$error \n";
+        refreshScreen();
+      });
+    } else {
       await showNoPermissionDialog();
     }
   }
 
   void onConnectDevice(index) {
     _currentConnectionStream = flutterReactiveBle.connectToAdvertisingDevice(
-      id:_foundBleUARTDevices[index].id,
+      id: _foundBleUARTDevices[index].id,
       prescanDuration: Duration(seconds: 1),
       withServices: [_UART_UUID, _UART_RX, _UART_TX],
     );
@@ -153,7 +175,7 @@ class _MyHomePageState extends State<MyHomePage> {
     refreshScreen();
     _connection = _currentConnectionStream.listen((event) {
       var id = event.deviceId.toString();
-      switch(event.connectionState) {
+      switch (event.connectionState) {
         case DeviceConnectionState.connecting:
           {
             _logTexts = "${_logTexts}Connecting to $id\n";
@@ -165,14 +187,21 @@ class _MyHomePageState extends State<MyHomePage> {
             _logTexts = "${_logTexts}Connected to $id\n";
             _numberOfMessagesReceived = 0;
             _receivedData = [];
-            _txCharacteristic = QualifiedCharacteristic(serviceId: _UART_UUID, characteristicId: _UART_TX, deviceId: event.deviceId);
-            _receivedDataStream = flutterReactiveBle.subscribeToCharacteristic(_txCharacteristic);
+            _txCharacteristic = QualifiedCharacteristic(
+                serviceId: _UART_UUID,
+                characteristicId: _UART_TX,
+                deviceId: event.deviceId);
+            _receivedDataStream =
+                flutterReactiveBle.subscribeToCharacteristic(_txCharacteristic);
             _receivedDataStream.listen((data) {
-               onNewReceivedData(data);
+              onNewReceivedData(data);
             }, onError: (dynamic error) {
               _logTexts = "${_logTexts}Error:$error$id\n";
             });
-            _rxCharacteristic = QualifiedCharacteristic(serviceId: _UART_UUID, characteristicId: _UART_RX, deviceId: event.deviceId);
+            _rxCharacteristic = QualifiedCharacteristic(
+                serviceId: _UART_UUID,
+                characteristicId: _UART_RX,
+                deviceId: event.deviceId);
             break;
           }
         case DeviceConnectionState.disconnecting:
@@ -193,147 +222,127 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const Text("BLE UART Devices found:"),
-            Container(
-                margin: const EdgeInsets.all(3.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.blue,
-                  width:2
-                )
-              ),
-              height: 100,
-              child: ListView.builder(
-                  itemCount: _foundBleUARTDevices.length,
-                  itemBuilder: (context, index) => Card(
-                        child: ListTile(
-                          dense: true,
-                          enabled: !((!_connected && _scanning) || (!_scanning && _connected)),
-                          trailing: GestureDetector(
-                            behavior: HitTestBehavior.translucent,
-                            onTap: () {
-                              (!_connected && _scanning) || (!_scanning && _connected)? (){}: onConnectDevice(index);
-                            },
-                            child: Container(
-                              width: 48,
-                              height: 48,
-                              padding: const EdgeInsets.symmetric(vertical: 4.0),
-                              alignment: Alignment.center,
-                              child: const Icon(Icons.add_link),
-                            ),
-                          ),
-                          subtitle: Text(_foundBleUARTDevices[index].id),
-                          title: Text("$index: ${_foundBleUARTDevices[index].name}"),
-                    ))
-              )
-            ),
-            const Text("Status messages:"),
-            Container(
-                margin: const EdgeInsets.all(3.0),
-               width:1400,
-               decoration: BoxDecoration(
-               borderRadius: BorderRadius.circular(10),
-               border: Border.all(
-                  color: Colors.blue,
-                  width:2
-                  )
-               ),
-               height: 90,
-               child: Scrollbar(
-
-                   child: SingleChildScrollView(
-                      child: Text(_logTexts)
-               )
-               )
-            ),
-            const Text("Received data:"),
-            Container(
-                margin: const EdgeInsets.all(3.0),
-                width:1400,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                        color: Colors.blue,
-                        width:2
-                    )
-                ),
-                height: 90,
-                child: Text(_receivedData.join("\n"))
-            ),
-            const Text("Send message:"),
-            Container(
-                margin: const EdgeInsets.all(3.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                        color: Colors.blue,
-                        width:2
-                    )
-                ),
-                child: Row(
-                    children: <Widget> [
-                      Expanded(
-                          child: TextField(
-                            enabled: _connected,
-                            controller: _dataToSendText,
-                            decoration: const InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Enter a string'
-                          ),
-                        )
-                      ),
-                      RaisedButton(
-                          child: Icon(
-                            Icons.send,
-                            color:_connected ? Colors.blue : Colors.grey,
-                          ),
-                          onPressed: _connected ? _sendData: (){}
-                      ),
-                    ]
-            ))
-           ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      persistentFooterButtons: [
-        Container(
-          height: 35,
+        body: SingleChildScrollView(
           child: Column(
-            children: [
-              if (_scanning) const Text("Scanning: Scanning") else const Text("Scanning: Idle"),
-              if (_connected) const Text("Connected") else const Text("disconnected."),
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const Text("BLE UART Devices found:"),
+              Container(
+                  margin: const EdgeInsets.all(3.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.blue, width: 2)),
+                  height: 100,
+                  child: ListView.builder(
+                      itemCount: _foundBleUARTDevices.length,
+                      itemBuilder: (context, index) => Card(
+                              child: ListTile(
+                            dense: true,
+                            enabled: !((!_connected && _scanning) ||
+                                (!_scanning && _connected)),
+                            trailing: GestureDetector(
+                              behavior: HitTestBehavior.translucent,
+                              onTap: () {
+                                (!_connected && _scanning) ||
+                                        (!_scanning && _connected)
+                                    ? () {}
+                                    : onConnectDevice(index);
+                              },
+                              child: Container(
+                                width: 48,
+                                height: 48,
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4.0),
+                                alignment: Alignment.center,
+                                child: const Icon(Icons.add_link),
+                              ),
+                            ),
+                            subtitle: Text(_foundBleUARTDevices[index].id),
+                            title: Text(
+                                "$index: ${_foundBleUARTDevices[index].name}"),
+                          )))),
+              const Text("Status messages:"),
+              Container(
+                  margin: const EdgeInsets.all(3.0),
+                  width: 1400,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.blue, width: 2)),
+                  height: 90,
+                  child: Scrollbar(
+                      child: SingleChildScrollView(child: Text(_logTexts)))),
+              const Text("Received data:"),
+              Container(
+                  margin: const EdgeInsets.all(3.0),
+                  width: 1400,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.blue, width: 2)),
+                  height: 90,
+                  child: Text(_receivedData.join("\n"))),
+              const Text("Send message:"),
+              Container(
+                  margin: const EdgeInsets.all(3.0),
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.blue, width: 2)),
+                  child: Row(children: <Widget>[
+                    Expanded(
+                        child: TextField(
+                      enabled: _connected,
+                      controller: _dataToSendText,
+                      decoration: const InputDecoration(
+                          border: InputBorder.none, hintText: 'Enter a string'),
+                    )),
+                    IconButton(
+                        icon: Icon(
+                          Icons.send,
+                          color: _connected ? Colors.blue : Colors.grey,
+                        ),
+                        onPressed: _connected ? _sendData : () {}),
+                  ]))
             ],
-          ) ,
-        ),
-        RaisedButton(
-          onPressed: !_scanning && !_connected ? _startScan : (){},
-          child: Icon(
-            Icons.play_arrow,
-            color: !_scanning && !_connected ? Colors.blue: Colors.grey,
           ),
         ),
-        RaisedButton(
-          onPressed: _scanning ? _stopScan: (){},
-          child: Icon(
-            Icons.stop,
-            color:_scanning ? Colors.blue: Colors.grey,
-          )
-        ),
-        RaisedButton(
-            onPressed: _connected ? _disconnect: (){},
-            child: Icon(
-              Icons.cancel,
-              color:_connected ? Colors.blue:Colors.grey,
-        )
-        )
-      ],
-    );
+        persistentFooterButtons: [
+          Container(
+            height: 35,
+            child: Column(
+              children: [
+                if (_scanning)
+                  const Text("Scanning: Scanning")
+                else
+                  const Text("Scanning: Idle"),
+                if (_connected)
+                  const Text("Connected")
+                else
+                  const Text("disconnected."),
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: !_scanning && !_connected ? _startScan : () {},
+            icon: Icon(
+              Icons.play_arrow,
+              color: !_scanning && !_connected ? Colors.blue : Colors.grey,
+            ),
+          ),
+          IconButton(
+              onPressed: _scanning ? _stopScan : () {},
+              icon: Icon(
+                Icons.stop,
+                color: _scanning ? Colors.blue : Colors.grey,
+              )),
+          IconButton(
+              onPressed: _connected ? _disconnect : () {},
+              icon: Icon(
+                Icons.cancel,
+                color: _connected ? Colors.blue : Colors.grey,
+              ))
+        ],
+      );
 }
